9a348f893e4b7c5b9a2855ce64b7717f4fb83a1f
Stefan <stefan@fourtonfish.com>
2013-11-23 01:02:19 +0100

    Added support for user's default language, IP used for fallback

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ HelloSalut: Hello as a Service
 
 Say hello to your visitors in their native language!
 
-1. Get your visitor's IP using your preferred method
+1. Get your visitor's default language and IP using your preferred methods
 2. Make a call to HelloSalut
 3. Say hello!
 
-The service is currently in beta while I'm adding more countries. For a live example and more information, please see http://hellosalut-ftfish.rhcloud.com/hello/
+The service is currently in beta while I'm adding more languages and countries. For a live example and more information, please see http://hellosalut-ftfish.rhcloud.com/hello/
 
 Note: I am using a modified database from http://www.ip2nation.com/
--- index.php
+++ index.php
@@ -1,43 +1,95 @@
 <?php
-  if (!isset($_REQUEST['ip'])) {
-    echo "Please see <a href='http://hellosalut-ftfish.rhcloud.com/hello/'>http://hellosalut-ftfish.rhcloud.com/hello/</a> for details on how to use this service.";
-  }
-  else{
-    $ip = $_REQUEST['ip'];
-    $ip = sprintf("%u\n", ip2long($ip));
-    //Example site is hosted on Openshift, so you will need to update the connection info below to point to your database
-    $connection = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "hellosalut", getenv('OPENSHIFT_MYSQL_DB_PORT'));
 
-    if (mysqli_connect_errno($connection)){
-      echo "Failed to connect to the database: " . mysqli_connect_error();
-    }
-    $query = "SELECT 
-              cc.code,
-              cc.country,
-              cc.hello
-            FROM 
-                ip2nationCountries cc,
-                ip2nation ip 
-            WHERE 
-                ip.ip < ? 
-                AND 
-                cc.code = ip.country 
-            ORDER BY 
-                ip.ip DESC
-            LIMIT 0,1";
+$hellosalut = array(
+	"code" => "none",
+	"country" => "none",
+	"hello" => "Hello"
+);
 
-    $stmt = $connection->prepare($query);
-    $stmt->bind_param("i", $ip);
-    $stmt->execute();
-    $stmt->bind_result($code, $country, $hello);
-    $stmt->fetch();
+function getHelloFromLang($lang) {
+		$connection = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "hellosalut", getenv('OPENSHIFT_MYSQL_DB_PORT'));
 
-    $hellosalut = array(
-      "code" => $code,
-      "country" => $country,
-      "hello" => $hello
-    );
+		if (mysqli_connect_errno($connection)){
+			echo "Failed to connect to the database: " . mysqli_connect_error();
+		}
 
-    echo json_encode($hellosalut);
-  }
+		$query = "SELECT 
+				cc.country,
+				cc.hello
+			FROM 
+				ip2nationCountries cc
+			WHERE 
+				cc.language = ?";
+		$stmt = $connection->prepare($query);
+		$stmt->bind_param("s", $lang);
+		$stmt->execute();
+		$stmt->bind_result($country, $hello);
+		$stmt->fetch();
+
+		if (!is_null($hello)){
+			global $hellosalut;
+
+			$hellosalut["code"] = $lang;
+			$hellosalut["country"] = $country;
+			$hellosalut["hello"] = $hello;
+		}
+	}
+
+
+function getHelloFromIP($ipRaw) {
+		$ip = sprintf("%u\n", ip2long($ipRaw));
+		$connection = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "hellosalut", getenv('OPENSHIFT_MYSQL_DB_PORT'));
+
+		if (mysqli_connect_errno($connection)){
+			echo "Failed to connect to the database: " . mysqli_connect_error();
+		}
+
+		$query = "SELECT 
+				cc.code,
+				cc.country,
+				cc.hello
+			FROM 
+				ip2nationCountries cc,
+				ip2nation ip 
+			WHERE 
+				ip.ip < ? 
+			AND 
+				cc.code = ip.country 
+			ORDER BY 
+				ip.ip DESC
+			LIMIT 0,1";
+
+		$stmt = $connection->prepare($query);
+		$stmt->bind_param("i", $ip);
+		$stmt->execute();
+		$stmt->bind_result($code, $country, $hello);
+		$stmt->fetch();
+
+
+		if (!is_null($hello)){
+			global $hellosalut;
+
+			$hellosalut["code"] = $code;
+			$hellosalut["country"] = $country;
+			$hellosalut["hello"] = $hello;
+		}
+	}
+ 
+if (!isset($_REQUEST['ip']) && !isset($_REQUEST['lang'])) {
+	echo "Please see <a href='http://hellosalut-ftfish.rhcloud.com/hello/'>http://hellosalut-ftfish.rhcloud.com/hello/</a> for details on how to use this service.";
+}
+else{
+	if (isset($_REQUEST['lang'])) {
+		getHelloFromLang($_REQUEST['lang']);
+	}
+	else{
+		if (isset($_REQUEST['ip'])) {
+			getHelloFromIP($_REQUEST['ip']);
+		}
+	}
+	echo json_encode($hellosalut);	
+}
+
+
+
 ?>
